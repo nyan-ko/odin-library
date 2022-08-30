1d001760b5c18d957ac328082064c92b35ee4037
nyan-ko <dumbstupidcatboy@gmail.com>
2022-08-30 18:14:56 -0400

    Add form submission and handling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.querySelector(`button[name="add"]`).addEventListener('click', showForm);
+document.querySelector(`form`).addEventListener('submit', addBook);
 
 const overlay = document.querySelector(".overlay");
 const form = document.querySelector(".form");
@@ -17,16 +18,32 @@ function Book (title, author, pages, read) {
     this.read = read;
 }
 
-function addBook(book) {
+function addBook(e) {
+    e.preventDefault();
+    let form = this;
+
+    let book = new Book(form.title.value, form.author.value, form.pages.value, form.read.value);
+    pushBook(book);
+    display();
+
+    hideForm();
+    form.reset();
+}
+
+function pushBook(book) {
     library.push(book);
 }
 
 function display() {
     const books = document.querySelector(".library");
+    books.replaceChildren();
     for (let i = 0; i < library.length; i++) {
         const book = document.createElement("div");
+        let content = library[i];
+
         book.classList.add("book");
-        book.textContent = library[i];
+        book.textContent = `${content.title} ${content.author} ${content.pages} ${content.read}`;
+
         books.appendChild(book);
     }
 }
@@ -38,5 +55,3 @@ function showForm(e) {
 function hideForm(e) {
     overlay.classList.add("invisible");
 }
-
-display();
